ff9138c0e492840473bf22d43060701396cec6e2
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-02-28 22:29:44 -0600

    comments

--- A2DuncanK.appstudio/forms/ifElse/ifElse.js
+++ A2DuncanK.appstudio/forms/ifElse/ifElse.js
@@ -1,8 +1,11 @@
 // Using If ... Else 
 
+// get values from user
 let firstName = prompt("Please enter your first name:")
 let stateAbbr = prompt("Please enter the two letter abbreviation for the state in which you are currently located (ex: NE for Nebraska):")
 let tempF = prompt("Please enter the current temperature outside in degrees Fahrenheit:")
+
+// set messages
 const messages = [
   "wear a warm coat, hat, scarf and gloves.",
   "wear a warm coat but you won't need a hat, scarf or gloves.",
@@ -10,9 +13,11 @@ const messages = [
   "wear a warm coat, hat and gloves. Maybe a scarf too."
   ]
 
+// if temperature is not below 32, check if below 50, then 70
 if (tempF < 32) {
     console.log(`${firstName}, because it's ${tempF} degrees Fahrenheit, you should ${messages[0]}`)
 } else if (tempF < 50) {
+    // check state
     if (stateAbbr == 'NE') {
         console.log(`${firstName}, because it's ${tempF} degrees Fahrenheit, you should ${messages[1]}`)
     } else if (stateAbbr == 'FL') {
--- A2DuncanK.appstudio/forms/switch/switch.js
+++ A2DuncanK.appstudio/forms/switch/switch.js
@@ -1,19 +1,26 @@
 // Switch Case
 
+// get values from user 
 let firstName = prompt("Please enter your first name:")
 let stateAbbr = prompt("Please enter the two letter abbreviation for the state in which you are currently located (ex: NE for Nebraska):")
 let tempF = prompt("Please enter the current temperature outside in degrees Fahrenheit:")
+
+// set messages
 const messages = [
   "wear a warm coat, hat, scarf and gloves.",
   "wear a warm coat but you won't need a hat, scarf or gloves.",
   "wear your warmest coat, a warm hat, a scarf, and warm gloves.",
   "wear a warm coat, hat and gloves. Maybe a scarf too."
   ]
-
+  
+// start switch
 switch (true) {
+    // first case
     case tempF < 32 && stateAbbr == 'NE':
+        // print message to console
         console.log(`${firstName}, because it's ${tempF} degrees Fahrenheit, you should ${messages[0]}`)
         break;
+    // second case
     case tempF > 50 && tempF < 70 && stateAbbr == 'FL': 
         console.log(`${firstName}, because it's ${tempF} degrees Fahrenheit, you should ${messages[3]}`)
         break;
